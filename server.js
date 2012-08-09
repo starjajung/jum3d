d74c341039f226eb74744ba4f9f428f4e045fe11
O JAE YOUNG <ki-kongboy@hanmail.net>
2012-08-09 11:29:25 +0900

    file server 예제
    file server 예제

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
-﻿var http = require('http'); // import java.lang... 
-var fs = require('fs');
-
-var server = http.createServer( function(req,res){ 
-	console.log("call from http client that is a browser");
-	console.log(req.url);
-	
-	// 파일 읽기 file 
-	if( req.url == '/')
-		req.url = './index.html';
-		
-	fs.readFile('.' + req.url, function (err, data) {
-		if (err) {
-			//res.writeHead(404,{'Content-Type':'text/html'}); 
-			throw err;
-		}
-		//console.log(data);
-		res.writeHead(200,{'Content-Type':'text/html'}); 
-		res.end(data); 
-	});
-});
-
-server.listen(52273,"10.0.1.81"); 
-
-console.log("Server running at http://10.0.1.81:52273/");
+﻿var util = require("util"),
+        http = require("http"),
+        url = require("url"),
+        path = require("path"),
+        fs = require("fs");
 
 
+http.createServer(function (request, response) {
+        var uri = url.parse(request.url).pathname;
+        var filename = path.join(process.cwd(), uri);
+        util.puts("filename : " + filename);
+        path.exists(filename, function(exists){
+                if(!exists){
+                        response.sendHeader(404,{"Content-type":"text/plain"} );
+                        response.write("404 Not Found\n");
+                        response.close();
+                        return;
+                }
+
+                fs.readFile(filename, function(err, data){
+                        if(err){
+                                console.log("error!!!");
+                                response.writeHead(500, {"Content-type":"text/plain"});
+                                response.write(err + "\n");
+                                response.end();
+                                return;
+                        }
+
+                        util.puts("data : " + data);
+                        response.statusCode = 200;
+                        response.write(data, "binary");
+                        response.end();
+                });
+        });
+}).listen(8080);
